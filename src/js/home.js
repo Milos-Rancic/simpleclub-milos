f31ba428ed1c593f6c8cf2d0b57d3e593263b904
Milos Rancic <milos.rancic@madetight.com>
2024-10-05 09:41:35 +0200

    innitial commit

---
+++ src/js/home.js
@@ -0,0 +1,8 @@
+// CSS import
+import '../css/home.scss'
+
+// JS import
+import './components/featureSlider.js'
+import './components/navigationMenu.js'
+import './components/smartAnimation.js'
+import './components/testimonialSlider.js'
